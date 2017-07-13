a3572e7ab6fa328340260bcde5755a54155955e8
Łukasz Lewczyński <lukaslew@users.noreply.github.com>
2017-07-13 13:55:02 +0200

    Back to dev IP address

--- deployment/dev_env/deploy_to_dev_env.sh
+++ deployment/dev_env/deploy_to_dev_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="lmis-dev.health.gov.mw:2376"
+export DOCKER_HOST="34.228.43.70:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env.sh
