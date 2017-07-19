fffe03ed28d24e49eab2f8dfc422cbedf35e2ecf
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2017-07-19 18:28:16 +0200

    Revert "Back to old UAT instance"
    This reverts commit bfa19d97483e18df26c47f2d8ec7fe31b67ed0fc.

--- deployment/uat_env/deploy_to_uat_env.sh
+++ deployment/uat_env/deploy_to_uat_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="34.249.116.186:2376"
+export DOCKER_HOST="lmis-uat.health.gov.mw:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env.sh
