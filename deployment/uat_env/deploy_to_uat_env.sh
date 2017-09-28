36fe40f4596a40aa2ffab51786cdc2d6deb32ab3
Jakub Kondrat <jkondrat@soldevelo.com>
2017-09-28 17:52:45 +0200

    MW-596: Fix after_restore.sh not being found by docker

--- deployment/uat_env/deploy_to_uat_env.sh
+++ deployment/uat_env/deploy_to_uat_env.sh
@@ -5,14 +5,14 @@ export DOCKER_HOST="lmis-uat.health.gov.mw:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials"
 
 ../shared/init_env.sh
-cp ../../credentials/.env-restore ./.env-restore
+cp ../../credentials/.env-restore restore/.env-restore
 
 ../shared/pull_images.sh $1
 
 if [ "$KEEP_OR_WIPE" == "wipe" ]; then
     echo "Restoring database from the latest snapshot"
     /usr/local/bin/docker-compose down
-    /usr/local/bin/docker-compose -f docker-compose-restore.yml run rds-restore
+    /usr/local/bin/docker-compose -f restore/docker-compose.yml run rds-restore
     unset spring_profiles_active
     /usr/local/bin/docker-compose up --build --force-recreate -d
 else
