01ca76aa80f17bb0cdf5481feb3f957f4ef54c6a
Jakub Kondrat <jkondrat@soldevelo.com>
2017-09-28 15:33:28 +0200

    MW-596: Allow to restore UAT's database from the latest snapshot

---
+++ deployment/uat_env/after_restore.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+: ${DATABASE_URL:?"Need to set DATABASE_URL"}
+: ${POSTGRES_USER:?"Need to set POSTGRES_USER"}
+: ${POSTGRES_PASSWORD:?"Need to set POSTGRES_PASSWORD"}
+
+: ${ENCODED_USER_PASSWORD:?"Need to set ENCODED_USER_PASSWORD"}
+: ${CLIENT_SECRET:?"Need to set CLIENT_SECRET"}
+: ${SERVICE_CLIENT_SECRET:?"Need to set SERVICE_CLIENT_SECRET"}
+
+URL=`echo ${DATABASE_URL} | sed -E 's/^jdbc\:(.+)/\1/'` # jdbc:<url>
+: "${URL:?URL not parsed}"
+
+sql=$(cat <<EOF
+UPDATE auth.auth_users SET password = '${ENCODED_USER_PASSWORD}';
+UPDATE referencedata.users SET email = NULL;
+UPDATE auth.oauth_client_details SET clientid = 'uat-service-client', clientsecret = '${SERVICE_CLIENT_SECRET}' WHERE clientid = 'production-service-client';
+UPDATE auth.oauth_client_details SET clientsecret = '${CLIENT_SECRET}' WHERE clientid = 'malawi-client';
+EOF
+)
+
+PGPASSWORD="${POSTGRES_PASSWORD}" psql ${URL} -U ${POSTGRES_USER} -c "$sql"
--- deployment/uat_env/deploy_to_uat_env.sh
+++ deployment/uat_env/deploy_to_uat_env.sh
@@ -5,7 +5,16 @@ export DOCKER_HOST="lmis-uat.health.gov.mw:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials"
 
 ../shared/init_env.sh
+cp ../../credentials/.env-restore ./.env-restore
 
 ../shared/pull_images.sh $1
 
-../shared/restart.sh $1
+if [ "$KEEP_OR_WIPE" == "wipe" ]; then
+    echo "Restoring database from the latest snapshot"
+    /usr/local/bin/docker-compose down
+    docker-compose -f docker-compose-restore.yml run rds-restore
+    unset spring_profiles_active
+    /usr/local/bin/docker-compose up --build --force-recreate -d
+else
+    ../shared/restart.sh $1
+fi
