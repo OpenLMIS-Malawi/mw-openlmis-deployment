30259391933c29b491d509529cb83c83be5f1e12
Łukasz Lewczyński <lukaslew@users.noreply.github.com>
2019-03-07 14:26:52 +0100

    Back to original values

--- deployment/prod_env/deploy_to_prod.sh
+++ deployment/prod_env/deploy_to_prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export DOCKER_TLS_VERIFY="0"
-export DOCKER_HOST="malawi-prod-elb-270164566.eu-west-1.elb.amazonaws.com:2376"
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="lmis.health.gov.mw:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials"
 
 ../shared/init_env.sh
