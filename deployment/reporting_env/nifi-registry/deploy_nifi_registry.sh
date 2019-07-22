40c16c8f8abef3f6c1ff6e102e1420dafca3860e
Łukasz Lewczyński <llewczynski@soldevelo.com>
2019-07-22 13:09:17 +0200

    MW4-3: Adjusted reporting stack to Malawi requirements

--- deployment/reporting_env/nifi-registry/deploy_nifi_registry.sh
+++ deployment/reporting_env/nifi-registry/deploy_nifi_registry.sh
@@ -3,8 +3,9 @@
 set -e
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
-export DOCKER_HOST="tcp://nifi-registry.openlmis.org:2376"
+export DOCKER_HOST="lmis-nifi-registry.health.gov.mw:2376"
+export DOCKER_CERT_PATH="${PWD}/../../credentials"
 
 /usr/local/bin/docker-compose kill
 /usr/local/bin/docker-compose down -v
-/usr/local/bin/docker-compose up --build --force-recreate -d
+/usr/local/bin/docker-compose up --build --force-recreate -d --scale scalyr=0
--- deployment/reporting_env/services/deploy_services.sh
+++ deployment/reporting_env/services/deploy_services.sh
@@ -2,18 +2,11 @@
 
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
-export DOCKER_HOST="tcp://nifi-registry.openlmis.org:2376"
+export DOCKER_HOST="lmis-nifi-registry.health.gov.mw:2376"
+export DOCKER_CERT_PATH="${PWD}/../../credentials"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
-export REPORTING_DIR_NAME=reporting
 
-export SUPERSET_ENABLE_SSL=false
-export SUPERSET_BEHIND_LOAD_BALANCER=true
-export SUPERSET_LOAD_BALANCER_REDIRECT_HTTP=true
-export SUPERSET_DOMAIN_NAME=superset.uat.openlmis.org
-export NIFI_ENABLE_SSL=false
-export NIFI_BEHIND_LOAD_BALANCER=true
-export NIFI_LOAD_BALANCER_REDIRECT_HTTP=true
-export NIFI_DOMAIN_NAME=nifi.uat.openlmis.org
+export REPORTING_DIR_NAME=reporting
 
 reportingRepo=$1
 
