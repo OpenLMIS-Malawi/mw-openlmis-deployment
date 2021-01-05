8aca8e193ee9166967ef36064bdfb29e75aa7f0a
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2021-01-05 14:34:58 +0100

    Revert "MALAWISUP-2482: Temporarily added --no-cache flag to docker-compose build command"
    This reverts commit 39ee43b9f342a75857ea2f8086fa71b8923a1de6.

--- deployment/prod_reporting_env/services/deploy_services.sh
+++ deployment/prod_reporting_env/services/deploy_services.sh
@@ -18,5 +18,4 @@ docker volume create pgdata
 cd "$reportingRepo/$REPORTING_DIR_NAME"
 $DOCKER_COMPOSE_BIN kill
 $DOCKER_COMPOSE_BIN down -v
-$DOCKER_COMPOSE_BIN build --no-cache
-$DOCKER_COMPOSE_BIN up --force-recreate -d
+$DOCKER_COMPOSE_BIN up --build --force-recreate -d
