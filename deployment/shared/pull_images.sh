e961b472a531dcd06db1ae5850ce69f9cf11b207
Mateusz Sobolak <msobolak@soldevelo.com>
2023-03-07 10:26:29 +0100

    Upgrade rsyslog to the latest version  (#5)
    * Update rsyslog to the latest version

--- deployment/shared/pull_images.sh
+++ deployment/shared/pull_images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker pull openlmis/rsyslog:1
+docker pull openlmis/rsyslog:2
 docker pull openlmis/postgres:9.6
 docker pull openlmis/nginx:1
 docker pull gliderlabs/consul-server
