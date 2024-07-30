5f6d10aaf8a4b81bf778e0664115c56b40fe620d
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2024-07-30 17:35:11 +0200

    Change DOCKER_HOST for dev_env

--- deployment/dev_env/deploy_to_dev_env.sh
+++ deployment/dev_env/deploy_to_dev_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="lmis-dev.health.gov.mw:2376"
+export DOCKER_HOST="malawi-dev-nlb-a49a014fcf8d8b5c.elb.us-east-1.amazonaws.com:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials"
 
 ../shared/init_env.sh
