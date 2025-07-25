2a5586c20cb420a392a206fcd2e3b509f7204bf1
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2025-07-25 10:50:27 +0200

    Update DOCKER_HOST and DOCKER_TLS_VERIFY for UAT

--- deployment/uat_env/deploy_to_uat_env.sh
+++ deployment/uat_env/deploy_to_uat_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export DOCKER_TLS_VERIFY="0"
-export DOCKER_HOST="52.214.239.50:2376"
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="lmis-uat.health.gov.mw:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials"
 
 ../shared/init_env.sh
