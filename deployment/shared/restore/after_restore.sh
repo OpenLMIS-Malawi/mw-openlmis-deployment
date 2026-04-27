1346f179a19bb36cb5f1d2760e10b356aa1ef416
Maciej Grochalski <mgrochalski@soldevelo.com>
2026-04-27 16:42:38 +0200

    Update after_restore.sh

--- deployment/shared/restore/after_restore.sh
+++ deployment/shared/restore/after_restore.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-: ${DATABASE_URL:?"Need to set DATABASE_URL"}
+: ${DB_HOST:?"Need to set DB_HOST"}
+: ${DB_PORT:?"Need to set DB_PORT"}
+: ${DB_NAME:?"Need to set DB_NAME"}
 : ${POSTGRES_USER:?"Need to set POSTGRES_USER"}
 : ${POSTGRES_PASSWORD:?"Need to set POSTGRES_PASSWORD"}
-
 : ${ENCODED_USER_PASSWORD:?"Need to set ENCODED_USER_PASSWORD"}
 : ${CLIENT_USERNAME:?"Need to set CLIENT_USERNAME"}
 : ${CLIENT_SECRET:?"Need to set CLIENT_SECRET"}
@@ -11,9 +12,6 @@
 : ${SERVICE_CLIENT_SECRET:?"Need to set SERVICE_CLIENT_SECRET"}
 : ${SUPERSET_SECRET:?"Need to set SUPERSET_SECRET"}
 : ${CLIENT_REDIRECT_URI:?"Need to set CLIENT_REDIRECT_URI"}
-
-URL=`echo ${DATABASE_URL} | sed -E 's/^jdbc\:(.+)/\1/'` # jdbc:<url>
-: "${URL:?URL not parsed}"
 
 sql=$(cat <<EOF
 UPDATE auth.auth_users SET password = '${ENCODED_USER_PASSWORD}';
@@ -26,8 +24,14 @@ UPDATE auth.oauth_client_details SET redirecturi = '${CLIENT_REDIRECT_URI}' WHER
 EOF
 )
 
-echo "Executing clearing sensitive data!"
+echo "Connecting to Host: ${DB_HOST}, Port: ${DB_PORT}, DB: ${DB_NAME} as ${POSTGRES_USER}"
+echo "Executing clearing sensitive data..."
 
-PGPASSWORD="${POSTGRES_PASSWORD}" psql ${URL} -U ${POSTGRES_USER} -c "$sql"
+PGPASSWORD="${POSTGRES_PASSWORD}" psql \
+    -h "${DB_HOST}" \
+    -p "${DB_PORT}" \
+    -d "${DB_NAME}" \
+    -U "${POSTGRES_USER}" \
+    -c "$sql"
 
-echo "Done clearing sensitive data!"
+echo "Success: Sensitive data cleared."
