f24f06e318ae8d63f80cefbf93b58796d49695b1
Maciej Grochalski <mgrochalski@soldevelo.com>
2026-04-30 10:50:17 +0200

    Rebuild restore-config image before running rds-restore

--- deployment/shared/restart_or_restore.sh
+++ deployment/shared/restart_or_restore.sh
@@ -5,6 +5,7 @@ if [ "$KEEP_OR_WIPE" == "wipe" ]; then
     cp ../../credentials/.env-restore ../shared/restore/.env-restore
 
     /usr/local/bin/docker-compose down -v
+    /usr/local/bin/docker-compose -f ../shared/restore/docker-compose.yml build restore-config
     /usr/local/bin/docker-compose -f ../shared/restore/docker-compose.yml run rds-restore
     /usr/local/bin/docker-compose up --build --force-recreate -d
 else
